5eefa3ccdf08211c38e7c600343a690554ec8fe2
calebjo <caleb.ri.jones@gmail.com>
2021-12-28 16:24:12 -0500

    Edit various reducers

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import * as Util from "./util/session_api_util.js"
+import * as APIUtil from "./util/session_api_util.js"
+import configureStore from "./store/store.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
   
-  window.signup = Util.signup;
-  window.login = Util.login;
-  window.logout = Util.logout;
+  window.signup = APIUtil.signup;
+  window.login = APIUtil.login;
+  window.logout = APIUtil.logout;
+
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+
 });
--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,7 +1,7 @@
 import { LOGOUT_CURRENT_USER, RECEIVE_CURRENT_USER } from "../actions/session_actions";
 
 const _nullSession = {
-    currentUser: null
+    id: null
 };
 
 const sessionReducer = (state = _nullSession, action) => {
@@ -9,8 +9,8 @@ const sessionReducer = (state = _nullSession, action) => {
 
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            const currentUser = action.user
-            return Object.assign({}, { currentUser })
+            const id = action.user.id
+            return Object.assign({}, { id })
         case LOGOUT_CURRENT_USER:
             return _nullSession;
         default:
