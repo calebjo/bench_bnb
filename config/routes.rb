03b531491ec63efd0d7d8e8f427d2a7fb12500bf
calebjo <caleb.ri.jones@gmail.com>
2021-12-28 10:39:01 -0500

    Adjust routes.rb and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'static_pages#root'
   
   namespace :api, defaults: {format: :json} do
-    resources :pokemon, only: [:create, :index, :show]
+    resources :users, only: [:create, :index, :show]
   end
 end
