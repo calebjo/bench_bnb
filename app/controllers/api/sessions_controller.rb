8f26e3029bbf125a4fdb6966ed141790ac0e0645
calebjo <caleb.ri.jones@gmail.com>
2021-12-28 10:42:36 -0500

    Hide master key

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,20 +7,20 @@ class Api::SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(
-            params[:user][:email],
+            params[:user][:password],
             params[:user][:password]
         )
         if @user
             login!(@user)
             redirect_to users_url
         else
-            flash.now[:errors] = ['Invalid email or password']
+            flash.now[:errors] = ['Invalid username or password']
             render :new
         end
     end
 
     def destroy
         logout!
-        redirect_to new_session_url
+        render {}
     end
 end
