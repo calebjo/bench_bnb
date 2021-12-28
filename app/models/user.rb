0f04d65ff7af22ccd81af321648dcaa9171c698d
calebjo <caleb.ri.jones@gmail.com>
2021-12-28 10:34:50 -0500

    Add frontend skeleton

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,40 @@
 class User < ApplicationRecord
-    validates :username, :password_digest, :session_token, presence: true
-    validates :username, :session_token, uniqueness: true
+    # validations
+    validates :username, :session_token, presence: true, uniqueness: true
+    validates :password_digest, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
 
-    after_initialize :ensure_session_token
+    has_many :notes
 
+    after_initialize :ensure_session_token
     attr_reader :password
+
+    def self.find_by_credentials(username, password)
+        @user = User.find_by(username: username)
+
+        if @user && @user.is_password?(password)
+            @user
+        else
+            nil
+        end
+    end
+
+    def is_password?(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def generate_session_token
+        SecureRandom::urlsafe_base64
+    end
 
     def password=(password)
         @password = password
         self.password_digest = BCrypt::Password.create(password)
     end
 
-    def generate_session_token
-        SecureRandom.urlsafe_base64
+    def ensure_session_token
+        self.session_token ||= generate_session_token
     end
 
     def reset_session_token!
@@ -21,22 +42,4 @@ class User < ApplicationRecord
         self.save!
         self.session_token
     end
-
-    def ensure_session_token
-        self.session_token ||= generate_session_token
-    end
-
-    def is_password?(password)
-        BCrypt::Password.new(self.password_digest).is_password?(password)
-    end 
-
-    def self.find_by_credentials(username, password)
-        user = User.find_by(username: username)
-        if user && user.is_password?(password)
-            user 
-        else 
-            nil 
-        end 
-    end
-
 end
