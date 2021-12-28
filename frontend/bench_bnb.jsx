21a03af103217945f39e439b76566282424fe06f
calebjo <caleb.ri.jones@gmail.com>
2021-12-28 12:23:11 -0500

    Add signup and login utils

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+import * as Util from "./util/session_api_util.js"
+
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
+  
+  window.signup = Util.signup;
+  window.login = Util.login;
+  window.logout = Util.logout;
 });
